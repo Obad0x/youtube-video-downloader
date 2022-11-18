d6d79831119ee829eda5b1e1823a63b8847af801
obaddabo <euphoria214rue@gmail.com>
2022-11-18 16:56:28 +0100

    changes

---
+++ Index.js
@@ -0,0 +1,31 @@
+
+const express = require ('express');
+const app = express();
+const ytdl = require("ytdl-core");
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+  res.render('index')
+})
+
+app.get("/download", async (req, res) => {
+	const v_id = req.query.url.split('v=')[1];
+    const info = await ytdl.getInfo(req.query.url);
+  
+
+	return res.render("download", {
+		url: "https://www.youtube.com/embed/" + v_id,
+        info: info.formats.sort((a, b) => {
+            return a.mimeType < b.mimeType;
+        }),
+	});
+});
+
+
+
+app.listen(port, ()=>{
+  console.log(`Server Started`);
+});
+
